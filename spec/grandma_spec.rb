b891a914319f04c12e219186ea58f185cf7db0cf
Dustin Blanton <dustinblntn@gmail.com>
2020-05-07 02:04:27 +0000

    Automatically backed up by Learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -1,11 +1,11 @@
 phrase="I LOVE YOU GRANDMA"
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+  if "#{phrase}"== "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
+  elsif "#{phrase}" == phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
-  else phrase == phrase.upcase
+  else "#{phrase}" == phrase.upcase
     puts "NO, NOT SINCE 1938!"
   end
     
